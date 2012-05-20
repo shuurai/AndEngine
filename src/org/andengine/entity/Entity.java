ed373d9a9552d1b9a17efbbe03fb9ab40c322f8d
player_03 <playernumberthree@gmail.com>
2012-05-19 20:31:09 -0400

    Added "getEntityModifierCount" and "getUpdateHandlerCount" to all entities.

--- src/org/andengine/entity/Entity.java
+++ src/org/andengine/entity/Entity.java
@@ -1017,6 +1017,14 @@ public class Entity implements IEntity {
 		}
 		return this.mUpdateHandlers.removeAll(pUpdateHandlerMatcher);
 	}
+	
+	@Override
+	public int getUpdateHandlerCount() {
+		if(this.mUpdateHandlers == null) {
+			return 0;
+		}
+		return this.mUpdateHandlers.size();
+	}
 
 	@Override
 	public int getUpdateHandlerCount() {
@@ -1056,6 +1064,14 @@ public class Entity implements IEntity {
 			return false;
 		}
 		return this.mEntityModifiers.removeAll(pEntityModifierMatcher);
+	}
+	
+	@Override
+	public int getEntityModifierCount() {
+		if(this.mEntityModifiers == null) {
+			return 0;
+		}
+		return this.mEntityModifiers.size();
 	}
 
 	@Override
